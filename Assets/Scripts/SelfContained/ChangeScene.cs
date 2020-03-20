0dfcb19e6e892455f13533798a721199549937f8
João Bueno <joaovictorbuenodecaldas@gmail.com>
2020-03-20 10:44:10 -0300

    Copied our most basic scripts over

---
+++ Assets/Scripts/SelfContained/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+
+    public void Change()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
